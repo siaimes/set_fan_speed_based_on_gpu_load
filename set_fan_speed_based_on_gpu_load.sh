5697a4f6ee24b96e7f905ddf1ca902649c667d1f
siaimes <34199488+siaimes@users.noreply.github.com>
2021-01-04 18:59:41 +0800

    性能优化，去除循环改用awk
    Signed-off-by: siaimes <34199488+siaimes@users.noreply.github.com>

--- set_fan_speed_based_on_gpu_load.sh
+++ set_fan_speed_based_on_gpu_load.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-GPUNUM=$(nvidia-smi -q | grep "GPU Current Temp" | wc -l)
-CPUNUM=$(ipmitool sdr list | grep -w TEMP_CPU. | wc -l)
 OLD=0
 while true; do
-    MAXTEMP=0
-    for i in $(seq 1 "$GPUNUM"); do
-        CUR=$(nvidia-smi -q | grep "GPU Current Temp" | awk '{print $5}' | sed -n "${i}p")
-        if [ "$CUR" -gt "$MAXTEMP" ]; then
-            MAXTEMP="$CUR"
-        fi
-    done
-    for i in $(seq 1 "$CPUNUM"); do
-        CUR=$(ipmitool sdr list | grep -w TEMP_CPU. | awk '{print $3}' | sed -n "${i}p")
-        if [ "$CUR" -gt "$MAXTEMP" ]; then
-            MAXTEMP="$CUR"
-        fi
-    done
-	echo $MAXTEMP
+    MAXTEMP=$({(nvidia-smi -q | grep "GPU Current Temp" | awk '{print $5}') && \
+	(ipmitool sdr list | grep -w TEMP_CPU. | awk '{print $3}')} | \
+	awk '{if(max=="") {max=$1}; if($1>max) {max=$1}} END {print max}')
 	if [ "$OLD" -ne "$MAXTEMP" ]; then
         eval "ipmitool raw 0x38 0x14 0x06 0x$MAXTEMP > /dev/null"
 		OLD=$MAXTEMP
