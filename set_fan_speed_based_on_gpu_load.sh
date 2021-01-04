060b4284b7a391ccf08449e1cc288e3c3eaaee5c
siaimes <34199488+siaimes@users.noreply.github.com>
2021-01-04 19:26:23 +0800

    制表位转空格
    Signed-off-by: siaimes <34199488+siaimes@users.noreply.github.com>

--- set_fan_speed_based_on_gpu_load.sh
+++ set_fan_speed_based_on_gpu_load.sh
@@ -3,11 +3,11 @@
 OLD=0
 while true; do
     MAXTEMP=$({(nvidia-smi -q | grep "GPU Current Temp" | awk '{print $5}') && \
-	(ipmitool sdr list | grep -w TEMP_CPU. | awk '{print $3}')} | \
-	awk '{if(max=="") {max=$1}; if($1>max) {max=$1}} END {print max}')
-	if [ "$OLD" -ne "$MAXTEMP" ]; then
+    (ipmitool sdr list | grep -w TEMP_CPU. | awk '{print $3}')} | \
+    awk '{if(max=="") {max=$1}; if($1>max) {max=$1}} END {print max}')
+    if [ "$OLD" -ne "$MAXTEMP" ]; then
         eval "ipmitool raw 0x38 0x14 0x06 0x$MAXTEMP > /dev/null"
-		OLD=$MAXTEMP
-	fi
+        OLD=$MAXTEMP
+    fi
     sleep 10s
 done
